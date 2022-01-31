8dcf20b5c39c2b758672d87a0dddbc6415084fe8
bg0trtv <tsvetan.krastev@festo.com>
2022-01-31 15:24:36 +0200

    removed def tests_ in read_print Python file

--- read_print.py
+++ read_print.py
@@ -12,23 +12,3 @@ def print_input(my_input):
     # for mock testing
     my_input = 5
     print(my_input)
-
-def test_read_input():
-    ''' a simple test read function '''
-    # for mock testing
-    # source: https://docs.python.org/3/library/unittest.mock.html
-    # m = MagicMock()
-    # p = PropertyMock(return_value=5)
-    # type(m).foo = p
-    # m.foo
-    # foo <--> read_input() ?!
-    test_input = 5
-    assert read_input() == test_input
-
-def test_print_input():
-    ''' a simple test print function '''
-    test_input = 5
-    #test_input = None
-    #assert print_input(read_input()) == test_input
-    assert print_input(5) == test_input
-    #assert print_input(test_input) is None
