8cc2f9ac3dc9a938248593cec402c1b4a7d5ef2e
TDK74 <98406593+TDK74@users.noreply.github.com>
2025-01-30 19:59:48 +0200

    unittest_load_tests.py
    Added new example Python Unit-test file:
    unittest_load_tests.py

---
+++ unittest_load_tests.py
@@ -0,0 +1,19 @@
+load_tests(loader, standard_tests, pattern)
+
+
+test_cases = (TestCase1, TestCase2, TestCase3)
+
+def load_tests(loader, tests, pattern):
+    suite = TestSuite()
+    for test_class in test_cases:
+        tests = loader.loadTestsFromTestCase(test_class)
+        suite.addTests(tests)
+    return suite
+
+
+def load_tests(loader, standard_tests, pattern):
+    # top level directory cached on loader instance
+    this_dir = os.path.dirname(__file__)
+    package_tests = loader.discover(start_dir=this_dir, pattern=pattern)
+    standard_tests.addTests(package_tests)
+    return standard_tests
