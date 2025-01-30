31ab6d0b1e486a8c7315adb43d7ba9405897b2ed
TDK74 <98406593+TDK74@users.noreply.github.com>
2025-01-30 20:17:24 +0200

    unittest_setup_teardown.py
    Added new example Python Unit-test file:
    unittest_setup_teardown.py

---
+++ unittest_setup_teardown.py
@@ -0,0 +1,17 @@
+import unittest
+
+class Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls._connection = createExpensiveConnectionObject()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._connection.destroy()
+        
+        
+def setUpModule():
+    createConnection()
+
+def tearDownModule():
+    closeConnection()
